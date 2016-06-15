b3babe82c953f2fa7a7943a7c3bd8a75a1f9b6b8
Noah Huppert <noahuppert@gmail.com>
2016-06-15 13:34:30 -0400

    +config docs -> readme,+defines routes from least to move specific

--- README.md
+++ README.md
@@ -6,8 +6,26 @@ A simple reverse proxy which emulates the behavior of a k8s cluster.
 k8svirt [application dir]
 ```
 
+## application directory
+Application directory should contain one or more service directories.
+
+## service directory
+Directory which contains a web service. Must contain a `k8svirt-config.json` file to specify behavior to k8svirt.
+
+# `k8svirt-config.json`
+A `k8svirt-config.json` file specifies a service's behavior for k8svirt.  
+
+The following keys are required to be present in the `k8svirt-config.json` file:
+- `path`
+	- Path service can be accessed by via the k8svirt proxy
+	- Example: `/v1/auth/google`
+- `server`
+	- The url service will be running on
+	- Must be a proper url
+		- *GOOD* `http://127.0.0.1:3000`
+		- *BAD* `127.0.0.1:3000`
+
 # building
 ```
-mkdir build
-make ..
+make build
 ```
--- k8svirt.go
+++ k8svirt.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"sort"
 )
 
 type K8sConfig struct {
@@ -18,9 +19,28 @@ type K8sConfig struct {
 	Server string `json:"server"`
 }
 
+type K8sConfigs []K8sConfig
+
+func (confs K8sConfigs) Len() int {
+	return len(confs)
+}
+
+// Determines if i should be before j
+func (confs K8sConfigs) Less(i, j int) bool {
+	ic := len(strings.Split(confs[i].Path, "/"))
+	jc := len(strings.Split(confs[j].Path, "/"))
+
+	return ic < jc
+}
+
+func (confs K8sConfigs) Swap(i, j int) {
+	confs[i], confs[j] = confs[j], confs[i]
+}
+
 var logger *log.Logger
+const LOGGER_PREFIX string = "[k8svirt] "
 const K8SVIRT_CONFIG_FILE_NAME string = "k8svirt-config.json"
-var appDef []K8sConfig
+var appDef K8sConfigs
 
 func RouteIndexHandler(w http.ResponseWriter, r *http.Request) {
 	logger.Printf("    [%v] %v => Index page", r.Method, r.URL)
@@ -30,7 +50,7 @@ func RouteIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	logger = log.New(os.Stdout, "[k8svirt] ", 0)
+	logger = log.New(os.Stdout, LOGGER_PREFIX, 0)
 	flag.Parse()
 
 	// Map application
@@ -47,18 +67,34 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", RouteIndexHandler)
+	sort.Sort(appDef)
 
+	logger.Printf("")
+	logger.Printf("Configuring routes")
 	for _, config := range appDef {
+		logger.Printf("    %v => %v", config.Path, config.Server)
 		serverUrl, _ := url.Parse(config.Server)
+
 		mux.HandleFunc(config.Path, func(w http.ResponseWriter, r *http.Request) {
 			logger.Printf("    [%v] %v => %v", r.Method, r.URL, config.Server)
-			httputil.NewSingleHostReverseProxy(serverUrl).ServeHTTP(w, r)
+
+			logger.SetPrefix(LOGGER_PREFIX + "        ")
+
+			proxy := httputil.NewSingleHostReverseProxy(serverUrl)
+			proxy.ErrorLog = logger
+			proxy.ServeHTTP(w, r)
+			
+			logger.SetPrefix(LOGGER_PREFIX)
 		})
 	}
 
+	logger.Printf("")
 	logger.Printf("Serving on :8080")
-	http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", mux)
+
+	if err != nil {
+		logger.Fatalf("Error starting server (error: %v)", err)
+	}
 }
 
 func walkFunc(path string, info os.FileInfo, err error) error {
